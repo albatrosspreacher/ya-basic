a47dc16d244d0c2c2265a99bb3c3e2c4c51ed002
Nandini Proothi <46650119+nandiniproothi@users.noreply.github.com>
2021-07-31 21:26:57 +0530

    added home page frontend :aerial_tramway:

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+// where your node app starts
+
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.sendFile(__dirname + "/views/dashboard.html");
+});
+
+app.get("/login", (req, res) => {
+  //perform some action
+})
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on localhost:" + listener.address().port);
+});
